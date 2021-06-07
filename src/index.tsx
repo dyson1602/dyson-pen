dc98779e4a6ef78cfbe4c5f148a9695f38a4f2b0
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-07 16:08:27 -0400

    added setCode() now applies result after code has been bundled

--- src/index.tsx
+++ src/index.tsx
@@ -30,7 +30,8 @@ const App = () => {
       plugins: [unpkgPathPlugin()],
     });
 
-    console.log(result);
+    // console.log(result);
+    setCode(result.outputFiles[0].text)
   };
 
   return (

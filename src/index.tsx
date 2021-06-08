ae671e3df3acf1aa0068ea22af3129c7fccf051f
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-07 22:35:17 -0400

    hold

--- src/index.tsx
+++ src/index.tsx
@@ -28,10 +28,14 @@ const App = () => {
       bundle: true,
       write: false,
       plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
     // console.log(result);
-    setCode(result.outputFiles[0].text)
+    setCode(result.outputFiles[0].text);
   };
 
   return (

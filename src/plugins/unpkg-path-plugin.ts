598a0a37bdb2432bbfefb8379f0c6cbb6dbcd348
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-09 15:14:16 -0400

    ADDED: checks cache for results; if results, then return results; else make fetch, set reults and return them

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,12 +1,24 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localForage from 'localforage';
+
+const fileCache = localForage.createInstance({
+  name: 'filecache',
+});
+
+(async () => {
+  await fileCache.setItem('color', 'red');
+
+  const color = await fileCache.getItem('color');
+
+  console.log(color);
+})();
 
 export const unpkgPathPlugin = () => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
         }
@@ -28,8 +40,6 @@ export const unpkgPathPlugin = () => {
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
-
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
@@ -38,14 +48,27 @@ export const unpkgPathPlugin = () => {
             `,
           };
         }
+        //Checks for search result in cache
+        const cachedResult = await fileCache.getItem(args.path);
 
+        //If result is in the cache, return it immediately
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        //If not, fetch the data
         const { data, request } = await axios.get(args.path);
 
-        return {
+        //Set cache
+        const result = {
           loader: 'jsx',
           contents: data,
           resolveDir: new URL('./', request.responseURL).pathname,
         };
+        await fileCache.setItem(args.path, result);
+
+        //Return results after caching them
+        return result;
       });
     },
   };

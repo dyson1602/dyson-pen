18989fb29a1d740adc5f3c097fd15d5a145fc776
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-13 16:52:35 -0400

    ADDED: Resizable package; new Resizable component to wrap CodeEditor and Preview in CodeCell

---
+++ src/components/resizable.tsx
@@ -0,0 +1,15 @@
+import { ResizableBox } from 'react-resizable';
+
+interface ResizableProps {
+  direction: 'horizonal' | 'vertical';
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return (
+    <ResizableBox height={300} width={300} resizeHandles={['s']}>
+      {children}
+    </ResizableBox>
+  );
+};
+
+export default Resizable;

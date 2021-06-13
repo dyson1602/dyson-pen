a7ff6f1335b89dca4578d4d895e5f207add412de
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-13 13:09:12 -0400

    ADDED: Bulma css package; fstyled Format button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -34,7 +34,12 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
 
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={editorDidMount}
         value={initialValue}

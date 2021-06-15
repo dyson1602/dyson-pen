33675e1483528865137a054f1a638948bf922fd3
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-15 13:41:29 -0400

    ADDED: conditional clicking for markdown editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,21 @@
 import MDEditor from '@uiw/react-md-editor';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  const editorRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
-    const listener = () => {
+    const listener = (event: MouseEvent) => {
+      if (
+        editorRef.current &&
+        event.target &&
+        editorRef.current.contains(event.target as Node)
+      ) {
+        console.log('element clicked is inside editor');
+        return;
+      }
+      console.log('element not in editor');
       setEditing(false);
     };
     document.addEventListener('click', listener, { capture: true });
@@ -17,7 +27,7 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div>
+      <div ref={editorRef}>
         <MDEditor />
       </div>
     );

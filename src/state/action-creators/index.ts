921e08cf284c6778f471fe6acf734627e75f90f7
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-15 18:22:16 -0400

    ADDED: action-creators fleshed out; CellMovementDirection type built

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,16 +1,52 @@
 import { ActionType } from '../action-types';
 import {
-  Action,
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
   InsertCellBeforeAction,
+  CellMovementDirection
 } from '../actions';
+import { CellTypes } from '../cell';
 
-export const updateCell = ():UpdateCellAction => {};
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
 
-export const deleteCell = ():DeleteCellAction => {};
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
 
-export const moveCell = ():MoveCellAction => {};
+export const moveCell = (
+  id: string,
+  direction: CellMovementDirection
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
 
-export const insertCellBefore = ():InsertCellBeforeAction => {};
+export const insertCellBefore = (
+  id: string,
+  type: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type,
+    },
+  };
+};

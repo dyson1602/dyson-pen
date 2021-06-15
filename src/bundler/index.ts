9ebcf8106dbddb0c6d9cfc698230be7c64a8957b
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-15 12:10:59 -0400

    CHANGED: bundler/index.ts results put in try/catch; returns object with code and error properties; useState() made for .err property

--- src/bundler/index.ts
+++ src/bundler/index.ts
@@ -2,30 +2,33 @@ import * as esbuild from 'esbuild-wasm';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 
-let service: esbuild.Service
+let service: esbuild.Service;
 
 const bundle = async (rawCode: string) => {
-  if(!service){
+  if (!service) {
     service = await esbuild.startService({
       worker: true,
       wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
     });
   }
-  
-  const result = await service.build({
-    entryPoints: ['index.js'],
-    bundle: true,
-    write: false,
-    //Plugins ran in order from left to right, so it is important to list them
-    //accordingly.
-    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-    define: {
-      'process.env.NODE_ENV': '"production"',
-      global: 'window',
-    },
-  });
 
-  return result.outputFiles[0].text
+  try {
+    const result = await service.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      //Plugins ran in order from left to right, so it is important to list them
+      //accordingly.
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+    return { code: result.outputFiles[0].text, err: '' };
+  } catch (err) {
+    return { code: '', err: err.message };
+  }
 };
 
-export default bundle
+export default bundle;

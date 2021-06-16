0803f0e1baf4338b01479f1db29101e7741276c1
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-15 22:57:52 -0400

    Cell List and CellListItems built

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,9 +2,25 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
+import { ActionType } from './action-types';
 
 export const store = createStore(
   reducers,
   {},
   composeWithDevTools(applyMiddleware(thunk))
 );
+
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'code',
+  },
+});
+store.dispatch({
+  type: ActionType.INSERT_CELL_BEFORE,
+  payload: {
+    id: null,
+    type: 'text',
+  },
+});

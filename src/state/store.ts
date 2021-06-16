347ee76bfeb213e2e4b10ccf17d68c5a1cba1498
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-15 22:25:55 -0400

    CLEANED: unused imports; test functions

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,19 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { ActionType } from './action-types';
 
 export const store = createStore(
   reducers,
   {},
   composeWithDevTools(applyMiddleware(thunk))
 );
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_BEFORE,
-  payload: {
-    id: null,
-    type: 'code',
-  },
-});
-console.log(store.getState());

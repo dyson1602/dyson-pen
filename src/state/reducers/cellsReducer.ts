ed5ec305bba9c90f8f6c15ccf1dfee38f105f1d4
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-17 14:08:42 -0400

    FIXED: incorrect placement of insertCellAfter

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -61,7 +61,7 @@ const reducer = produce(
         if (foundIndex < 0) {
           state.order.unshift(cell.id);
         } else {
-          state.order.splice(foundIndex, 0, cell.id);
+          state.order.splice(foundIndex + 1, 0, cell.id);
         }
 
         return;

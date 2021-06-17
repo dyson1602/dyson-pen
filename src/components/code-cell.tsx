f115b847253de5ad4a3fdd8c54394527730ed166
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-17 14:28:07 -0400

    CHANGED: esbuild properties to account for naming collisions on accidental import of React or ReactDOM by user

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -20,13 +20,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const orderedCells = order.map((id) => data[id]);
 
     const cumulativeCode = [
+      //_React and _ReactDOM used to avoid naming collisions if use imports
+      //either package not knowing they are autoloaded to work with jsx
       `
+        import _React from 'react';
+        import _ReactDOM from 'react-dom';
+        
         const show = (value) => {
+
           const root = document.querySelector('#root')
 
           if(typeof value === 'object'){
             if(value.$$typeof && value.props){
-              ReactDOM.render(value, root)
+              _ReactDOM.render(value, root)
             } else {
             root.innerHTML = JSON.stringify(value)
             }

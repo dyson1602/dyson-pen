7dcf62c816c0571d9909683730703ce1a62fc884
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-17 11:07:02 -0400

    FIXED: eslint-disable for bundle dependency

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -15,13 +15,20 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    console.log(bundle)
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
+
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
-    }, 1000);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (

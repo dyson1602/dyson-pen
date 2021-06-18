c40473a9d8035801cee9f384ff5b23736743a33a
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-18 18:01:10 -0400

    ADDED: cells routes; createCellsRouter function imported to local-api serve() function

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -7,6 +7,7 @@ exports.serve = void 0;
 var express_1 = __importDefault(require("express"));
 var http_proxy_middleware_1 = require("http-proxy-middleware");
 var path_1 = __importDefault(require("path"));
+var cells_1 = require("./routes/cells");
 var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
     if (useProxy) {
@@ -23,6 +24,7 @@ var serve = function (port, filename, dir, useProxy) {
         //path.dirname gives everything up to the file
         app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     }
+    app.use(cells_1.createCellsRouter(filename, dir));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });

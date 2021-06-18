2311ce614a130b03b495607f010d82bf547c08ec
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-18 19:18:43 -0400

    MOVED: createCellsRouter above useProxy conditional

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -10,6 +10,7 @@ var path_1 = __importDefault(require("path"));
 var cells_1 = require("./routes/cells");
 var serve = function (port, filename, dir, useProxy) {
     var app = express_1.default();
+    app.use(cells_1.createCellsRouter(filename, dir));
     if (useProxy) {
         app.use(http_proxy_middleware_1.createProxyMiddleware({
             target: 'http://localhost:3000',
@@ -24,7 +25,6 @@ var serve = function (port, filename, dir, useProxy) {
         //path.dirname gives everything up to the file
         app.use(express_1.default.static(path_1.default.dirname(packagePath)));
     }
-    app.use(cells_1.createCellsRouter(filename, dir));
     return new Promise(function (resolve, reject) {
         app.listen(port, resolve).on('error', reject);
     });

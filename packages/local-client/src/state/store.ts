4524c37c7f940fa5113d78594860a1fd790313ff
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-18 19:13:40 -0400

    CLEANED: template cell render

--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -2,28 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
-import { ActionType } from './action-types';
 
 export const store = createStore(
   reducers,
   {},
   composeWithDevTools(applyMiddleware(thunk))
 );
-
-//Template cells added for user to see on page load
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'text',
-  },
-});
-
-store.dispatch({
-  type: ActionType.INSERT_CELL_AFTER,
-  payload: {
-    id: null,
-    type: 'code',
-  },
-});
-

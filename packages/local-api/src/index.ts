83ca160023967d5ab12bbf6375a7410df443abf6
dyson1602 <cclark000@citymail.cuny.edu>
2021-06-18 17:44:57 -0400

    ADDED: conditional to decide if server is being started in development or on user's machine

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,16 +1,30 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      logLevel: 'silent',
-    })
-  );
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    //require.resolve() applies node's path resolution algorithm to figure out the
+    //file location of the html file
+    const packagePath = require.resolve('local-client/build/index.html');
+    //path.dirname gives everything up to the file
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
